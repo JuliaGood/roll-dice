c6180d1a4b4fe09d374461e3c9522294024d91d8
JuliaGood <gudak.yuliya@gmail.com>
2021-01-30 00:23:56 +0200

    state changing. dynamics on site. Lot of new changing

--- src/Rolldice.js
+++ src/Rolldice.js
@@ -3,12 +3,28 @@ import Die from './Die';
 import './Rolldice.css';
  
 class Rolldice extends Component{
+  static defaultProps = {
+    sides: ['one', 'two', 'three', 'four', 'five', 'six']
+  };
   constructor(props) {
     super(props);
     this.state = {
       die1: 'one',
       die2: 'one'
     }
+    this.roll = this.roll.bind(this);
+  }
+
+  roll() {
+    // pick = choose 2 new rolls (numbers)
+    const newDie1 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
+    const newDie2 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
+
+    // set = change new state with new rolls
+    this.setState({
+      die1: newDie1,
+      die2: newDie2,
+    })
   }
 
   render() {
@@ -16,6 +32,7 @@ class Rolldice extends Component{
         <div>
           <Die face={this.state.die1}/>
           <Die face={this.state.die2}/>
+          <button onClick={this.roll}>Roll Dice!</button>
         </div>
     )
   }
