1719fab1cf8da73435b1d0ba9c763e8248ccff6a
JuliaGood <gudak.yuliya@gmail.com>
2021-01-30 16:56:21 +0200

    adding Style as VARIABLE in component

--- src/Die.js
+++ src/Die.js
@@ -3,8 +3,9 @@ import './Die.css';
 
 class Die extends Component{
   render() {
+    let cls = `Die fas fa-dice-${this.props.face}`;
     return(
-        <i className={`Die fas fa-dice-${this.props.face}`}></i>
+        <i className={cls}></i>
     )
   }
 }
